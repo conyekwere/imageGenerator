18a1d824f01b011fd22f609d28f7b24df1dfb2b6
Your Name <you@example.com>
2022-06-26 08:40:35 -0700

    Initial Commit

---
+++ RandomGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomGenerator
+//
+//  Created by Chima onyekwere on 6/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
